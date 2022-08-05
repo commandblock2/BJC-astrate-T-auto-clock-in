349b92956fdc87cc03ac525441f827a98f46404b
commandblock2 <tony_hu@live.com>
2022-08-05 16:05:41 +0800

    init

---
+++ main.py
@@ -0,0 +1,48 @@
+import settings
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+def wait_until_complete(driver):
+    WebDriverWait(driver, 500).until(lambda x: driver.execute_script('return document.readyState == "complete"'))
+
+driver = webdriver.Firefox()
+
+driver.get("https://itsapp.bjut.edu.cn/uc/wap/login?redirect=https://itsapp.bjut.edu.cn/site/applicationSquare/index?sid=2")
+driver.switch_to.frame("loginIframe")
+with open("login") as file:
+    (username, password) = file.readline().split(" ")
+    wait_until_complete(driver)
+    driver.find_element(value="unPassword").send_keys(username)
+    driver.find_element(value="pwPassword").send_keys(password)
+
+driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[2]/div[2]/div/div[2]/div[2]/div/div[7]/input").click()
+
+
+driver.switch_to.parent_frame()
+wait_until_complete(driver)
+driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[1]/div/section/div/div[3]/div[1]/ul/li[2]").click()
+
+with open("coord") as file:
+    text = file.readline().replace('\n', '')
+    coord = file.readline().replace('\n', '')
+
+    post_string = """[{\\"question_id\\":48,\\"answer\\":{\\"id\\":111,\\"text\\":null}},{\\"question_id\\":36,\\"answer\\":{\\"id\\":71,\\"text\\":\\\"""" + text + """\\",\\"location\\":\\\"""" + coord + """\\"}},{\\"question_id\\":50,\\"answer\\":{\\"id\\":115,\\"text\\":null}},{\\"question_id\\":55,\\"answer\\":{\\"id\\":128,\\"text\\":null}},{\\"question_id\\":56,\\"answer\\":{\\"id\\":130,\\"text\\":null}},{\\"question_id\\":58,\\"answer\\":{\\"id\\":134,\\"text\\":null}},{\\"question_id\\":60,\\"answer\\":{\\"id\\":137,\\"text\\":null}},{\\"question_id\\":64,\\"answer\\":{\\"id\\":145,\\"text\\":null}},{\\"question_id\\":65,\\"answer\\":{\\"id\\":149,\\"text\\":null}},{\\"question_id\\":67,\\"answer\\":{\\"id\\":152,\\"text\\":null}},{\\"question_id\\":93,\\"answer\\":{\\"id\\":240,\\"text\\":null}},{\\"question_id\\":94,\\"answer\\":{\\"id\\":244,\\"text\\":null}},{\\"question_id\\":75,\\"answer\\":{\\"id\\":184,\\"text\\":null}},{\\"question_id\\":95,\\"answer\\":{\\"id\\":252,\\"text\\":null}}]"""
+
+    wait_until_complete(driver)
+    WebDriverWait(driver, 500).until(lambda x: driver.execute_script("return typeof uni !== 'undefined'"))
+    script = """
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://yqfk.bjut.edu.cn/api/home/daily_form");
+
+    bearer = uni.getStorageSync('token');
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Authorization", "Bearer " + bearer);
+    xhr.send("{}");
+    xhr.onerror = console.log;
+    xhr.onload = console.log;
+    console.log(xhr.responseText);
+    """.format(post_string).replace('\n', '')
+    driver.execute_script(script)
+
+# driver.close()
---
+++ settings.py
@@ -0,0 +1,3 @@
+login_url = "https://itsapp.bjut.edu.cn/uc/wap/login?redirect=https://itsapp.bjut.edu.cn/site/applicationSquare/index?sid=2"
+
+implicit_wait = 30
